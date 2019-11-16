aaedd521a85b312ad15eec9990f065995ed68436
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2019-11-16 10:44:49 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,48 @@
+from sys import getsizeof
+
+number = 123
+floating = 123.123
+name = "Abhi"
+#
+# print(number, name, floating)
+#
+# print("number " + str(getsizeof(number)))
+# print("number " + str(getsizeof(number * 1000)))
+
+number = number1 = 1
+print(number, number1)
+
+number, number1, number2, number3 = 1, 1, 2, 3
+
+print(number, number1,number2,number3)
+
+# Standard data types:
+#
+# Numbers
+# Strings
+# Lists
+# Tuples
+# Dictionary
+
+num1 = 1
+
+# del num1
+
+print(num1)
+
+# Three number data types:
+# int          float           complex
+# 1             0.0              1.1j
+# 2            15.15             5+5j
+# 3            32.e18
+# 0x68
+
+# Strings:
+
+string = "Hello World"
+
+print(string[-5])
+print(string[:7])
+
+print(string + string)
+print(string * 3)
---
+++ learn101.py
@@ -0,0 +1,70 @@
+print('THis is a string')
+
+a = 1123
+b = 12345
+c = "asd"
+
+# keywords:
+# and
+# not
+# exec
+# as
+# finally
+# or
+# assert
+# for
+# pass
+# break
+# from
+# print
+# class
+# global
+# raise
+# continue
+# if
+# elif
+#     else
+# def
+# return
+# import
+# try
+# del
+# in
+# while
+# is
+# with
+# lambda
+# yield
+# except
+#
+# Indentation:
+# if True:
+#
+#
+# else:
+#
+# Multiline statement:
+add = 1 + \
+      2 + 3 \
+      + 4
+print(add)
+
+# Multiline Comment:
+'''
+isadnaiss
+print(1)
+'''
+
+# Multiline string:
+abc = """
+A 
+hello
+what
+"""
+
+print(abc)
+
+a = input("Enter your name here:")
+print(a)
+    #
+    # suites
