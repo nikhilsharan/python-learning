da2257b17c2dc7f72d0ee70a08df01341520f0d3
nikhilsharan <40920498+nikhilsharan@users.noreply.github.com>
2019-11-17 09:33:42 +0530

    taken input from user and did type conversion

---
+++ day1-ex01.py
@@ -0,0 +1,16 @@
+myVariable = input("Enter number/word here : ") #always a string so if i want an integer to be my input??
+print(type(myVariable))
+
+print(int(myVariable))
+
+print(float(myVariable))
+
+print(set(myVariable))
+
+print(list(myVariable))
+
+print(tuple(myVariable))
+
+# print(chr(myVariable))  #TypeError: an integer is required (got type str)
+
+# print(dict(myVariable))
